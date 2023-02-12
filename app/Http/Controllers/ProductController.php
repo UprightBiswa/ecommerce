7da8250c5ebc65810db9baf2f71d605fc8b19876
UprightBiswa <djdas000000@gmail.com>
2023-02-12 15:38:38 +0530

    login session user auth kernel

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+    function index(){
+        return "Welcome to product page ";
+    }
+}
